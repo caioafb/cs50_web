a352fc17e901deffbd66b4981aac4b3315f8b4e9
caioafb <caioafb@gmail.com>
2024-02-09 12:39:03 -0300

    bug fixes

--- project2/commerce/auctions/models.py
+++ project2/commerce/auctions/models.py
@@ -23,11 +23,17 @@ class Listing(models.Model):
     image_url = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
+
 class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
     value = models.DecimalField(max_digits=7, decimal_places=2)
     date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.listing} - ${self.value}"
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
@@ -35,6 +41,12 @@ class Comment(models.Model):
     text = models.TextField()
     date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.user} on {self.date}"
+
 class Watchlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user}, listing {self.listing}"
